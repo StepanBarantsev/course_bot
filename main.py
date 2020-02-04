9f8154f633ed682de03555e45da91b69768b7aa7
Баранцев Степан <stepan.barantsev@gmail.com>
2020-02-04 10:36:56 +0300

    Revert "Подключил фауну"
    This reverts commit 6bb6463a5be9b35d5140d2eb7c064220d065ab00.

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 import telebot
 from settings import bot_token
-from settings import faunakey
-from faunadb import query
-from faunadb.objects import Ref
-from faunadb.client import FaunaClient
-
 
 bot = telebot.TeleBot(bot_token)
 
 
 @bot.message_handler(commands=['getdays'])
 def getdays(message):
-    clientf = FaunaClient(secret=faunakey)
-    result = clientf.query(query.get(query.match(query.index('telegram_id'), message.chat.id)))
-    bot.send_message(message.chat.id, str(result['data']['days'] + 1))
+    with open('database.txt') as db:
+        bot.send_message(message.chat.id, str(int(db.read()) + 1))
 
 
 if __name__ == '__main__':
--- send_script.py
+++ send_script.py
@@ -2,37 +2,21 @@ import telebot
 import schedule
 from settings import bot_token
 import time
-from settings import faunakey
-from faunadb import query
-from faunadb.objects import Ref
-from faunadb.client import FaunaClient
 
 
-def job(bot, clientf):
+def job(bot):
+    with open('database.txt') as db:
+        number = db.read()
+        bot.send_message(821086704, 'Осталось дней: ' + number)
+        number = int(number)
 
-    # Получаем все данные из бд
-    students = get_all_data_from_faunadb(clientf)
-
-    for student in students:
-        student = student['data']
-        bot.send_message(student['id'], 'Осталось дней: ' + str(student['days']))
-        new_days = student['days'] - 1
-        update_data(clientf, student, new_days)
-
-
-def update_data(clientf, element, new_days):
-    clientf.query(query.update(query.select('ref', query.get(query.match(query.index('telegram_id'), element['id']))),
-                               {'data': {'days': new_days}}))
-
-
-def get_all_data_from_faunadb(clientf):
-    return clientf.query(query.map_(query.lambda_("x", query.get(query.var("x"))),
-                                    query.paginate(query.match(query.index("all_students")))))['data']
+    with open('database.txt', 'w') as db:
+        db.write(str(number - 1))
 
 
 bot = telebot.TeleBot(bot_token)
-faunaclient = FaunaClient(secret=faunakey)
-schedule.every(5).seconds.do(job, bot, faunaclient)
+
+schedule.every(30).seconds.do(job, bot)
 
 while True:
     schedule.run_pending()
